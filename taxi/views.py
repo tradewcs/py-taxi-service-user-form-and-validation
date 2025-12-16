6ba2cd45fa2b12e1c58da0c5f03c29ca25fe6e59
Mike <tradewcs@gmail.com>
2025-12-16 15:01:06 +0200

    remove Driver import

--- taxi/views.py
+++ taxi/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Driver, Car, Manufacturer
+from .models import Car, Manufacturer
 from .forms import (
     DriverCreateForm,
     DriverUpdateForm,
